ff59eeb38572494902a2a73db48d61f8d0ade8f6
Andy Liang <andylikescodes@gmail.com>
2022-05-16 19:09:26 -0700

    bug fix for weight name

--- src/models.jl
+++ src/models.jl
@@ -7,7 +7,7 @@ function cal_ipw(data, adjustments, second_degree_terms, treatment)
 
     fit = glm(Term(Symbol(treatment)) ~ sum(Term.(Symbol.(adjustments))) + sum(Term.(Symbol.(second_degree_terms .* "^2"))), data, Binomial(), ProbitLink())
     pred = predict(fit, data)
-    data[!, "w"] = 1 .- pred
+    data[!, treatment*"_w"] = 1 .- pred
     data[data[!, treatment] .= 1, treatment*"_w"] = pred
     
     return fit
